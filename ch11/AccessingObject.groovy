74c23e4dae3e7671ce2909475dd61909cdfe4047
FreeBz <freebz@hananet.net>
2014-01-09 01:55:46 +0900

    Exploring Metaobject Protocal(MOP)

---
+++ ch11/AccessingObject.groovy
@@ -0,0 +1,20 @@
+def printInfo(obj) {
+  // Assume user entered these values from standard input
+  usrRequestedProperty = 'bytes'
+  usrRequestedMethod = 'toUpperCase'
+  
+  println obj[usrRequestedProperty]
+  //or
+  println obj."$usrRequestedProperty"
+
+  println obj."$usrRequestedMethod"()
+  //or
+  println obj.invokeMethod(usrRequestedMethod, null)
+}
+
+printInfo('hello')
+
+
+
+println "Properties of 'hello' are: "
+'hello'.properties.each { println it }
